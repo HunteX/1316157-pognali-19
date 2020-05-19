a34ce595bb451b526752131b94b7f66eb04df016
Andrey Alenin <huntex@yandex.ru>
2020-05-19 17:48:52 +0300

    навигация и кнопки

---
+++ source/js/navigation.js
@@ -0,0 +1,11 @@
+function goToIndex() {
+  document.location = '/index.html';
+}
+
+function goToForm() {
+  document.location = '/form.html';
+}
+
+function goToCatalog() {
+  document.location = '/catalog.html';
+}
